428cd0e89fe0c4707572169f132c27587c1dc9bb
RachelFurlong-dev <rachel.furlong@ekcgroup.ac.uk>
2022-04-11 13:40:29 +0000

    Create Add Plant template

--- app.py
+++ app.py
@@ -98,7 +98,12 @@ def logout():
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
-    
+
+
+@app.route("/add_plant")
+def add_plant():
+    return render_template("add_plant.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
